3e40173e68b5e1c899f85e794a85b4268349710f
ThePr0grammer <luka.dragar3@gmail.com>
2020-11-13 13:15:09 +0100

    Tabelar_2.0 now minimizing functions

--- Tabelar_2.0.py
+++ Tabelar_2.0.py
@@ -1,40 +1,40 @@
-
 import re
 from functools import partial
 import sys
+import Kmap
+import itertools
 
 save = True
 translator = {
 
-            "∧": " and ",
-            "&&": " and ",
-            "||": " or ",
-            "∨": " or ",
-            "+": " or ",
-            "¬": " not ",
-            "!": " not ",
-            ">>": " ^1 or ",
-            "⇒": " ^1 or ",
-            "⇔": " == ",
-            "==": " == ",
-            "*": " and "}
+    "∧": " and ",
+    "&&": " and ",
+    "||": " or ",
+    "∨": " or ",
+    "+": " or ",
+    "¬": " not ",
+    "!": " not ",
+    ">>": " ^1 or ",
+    "⇒": " ^1 or ",
+    "⇔": " == ",
+    "==": " == ",
+    "*": " and "}
 
-#realizacija da je (a xor 1)  enak kot (not a)
+# realizacija da je (a xor 1)  enak kot (not a)
 translatorforwords = {
-            "xor": " ^ ",#add xnor
-            "nor": " ^1 and 1^ ",
-            "nand": " ^1 or 1^ ",
-            "and": " and ",
-            "or": " or ",
-            "not": " not ",
-            "imply": " ^1 or ",
-            "equal": " == "
+    "xor": " ^ ",  # add xnor
+    "nor": " ^1 and 1^ ",
+    "nand": " ^1 or 1^ ",
+    "and": " and ",
+    "or": " or ",
+    "not": " not ",
+    "imply": " ^1 or ",
+    "equal": " == "
 
 }
 
 
 def replacewords(input):
-
     for wordkey in translatorforwords.keys():
         input = input.replace(wordkey, translatorforwords[wordkey])
 
@@ -42,65 +42,61 @@ def replacewords(input):
 
 
 def replacesigns(input):
-
     for key in translator.keys():
-       input= input.replace(key, translator[key])
+        input = input.replace(key, translator[key])
 
     return input
 
 
 def findvariables(exp):
-
     for wordkey in translatorforwords.keys():
         exp = exp.replace(wordkey, " ")
 
     for key in translator.keys():
         exp = exp.replace(key, " ")
 
-    exp=exp.replace("("," ").replace(")"," ")
+    exp = exp.replace("(", " ").replace(")", " ")
 
-    listofvar=exp.split()
+    listofvar = exp.split()
     listofvar.sort()
 
     mylist = list(dict.fromkeys(listofvar))
 
     return mylist
 
-def calculate(expression_list,variable_list):
 
-
-    result_dict={}
+def calculate(expression_list, variable_list):
+    result_dict = {}
 
     for expression in expression_list:
 
-        print("-"*30)
+        print("-" * 30)
         print(expression)
-        print("-"*30)
+        print("-" * 30)
 
-        expression_results=[]
+        expression_results = []
         for input_vector in range(0, 2 ** len(variable_list)):
 
             exp = expression
 
             minterm = list("{0:b}".format(input_vector).zfill(len(variable_list)))
-           
 
             cnt = 0
             for var in variable_list:
-                #replacing variables names with minterm values
-                exp=re.sub(r'\b{}\b'.format(var), minterm[cnt], exp) #replacing only whole words MATCHING
+                # replacing variables names with minterm values
+                exp = re.sub(r'\b{}\b'.format(var), minterm[cnt], exp)  # replacing only whole words MATCHING
                 cnt = cnt + 1
 
             result = int(eval(exp))
 
-            print(exp+" ~ "+str(result))
+            print(exp + " ~ " + str(result))
 
             expression_results.append(result)
 
-        result_dict[str(expression)]=(expression_results)
+        result_dict[str(expression)] = (expression_results)
 
     return result_dict
-            
+
 
 def printresults(variables_list, expression_list, results):
     print("--------------------Results------------------------------")
@@ -128,17 +124,90 @@ def printresults(variables_list, expression_list, results):
 
     print("-------------------------------------------------------")
     if save:
-        print("\n".join(tosave),  file=open('save.txt', 'w'))
+        print("\n".join(tosave), file=open('save.txt', 'w'))
+
+
+def get_minterms(variables_list, expression_list, results):
+    expressions_minterms_list = {}
+
+    for expression in expression_list:
+
+        minterms = []
+
+        for k in range(0, 2 ** len(variables_list)):
+            minterm = str(results[str(expression)][k]).replace("True", "1").replace("False", "0")
+
+            if minterm == "1":
+                input_vector = list("{0:b}".format(k).zfill(len(variables_list)))
+                minterms.append(input_vector)
+
+        expressions_minterms_list[str(expression)] = minterms
+
+    return expressions_minterms_list
+
+
+def printPDNO(minterms, variables_list):
+    for expression, minterm_list in minterms.items():
+
+        pdno = []
+        for minterm in minterm_list:
+
+            one_minterm = ""
+            for i in range(0, len(variables_list)):
+                if minterm[i] == "1":
+                    one_minterm += variables_list[i]
+
+                if minterm[i] == "0":
+                    one_minterm += variables_list[i] + "'"  # negation sign
+
+            pdno.append(one_minterm)
+
+        print("PDNO: " + expression + " \n " + " + ".join(pdno))
+
+
+def getDNO(minterms):
+    DNO = {}
+    for expression, minermlist in minterms.items():
+        mintermss = []
+        for minterms in minermlist:
+            mintermss.append("".join(minterms))
+
+        simplified = Kmap.simplifyyy(mintermss)
+
+        DNO[expression] = simplified
+
+    return DNO
+
+
+def printDNO(dno_list, variables_list):
+    for expression, dnos in dno_list.items():
+
+        res = []
+        for dno in dnos:
+            dno=str(dno)
+            one_minterm = ""
+            for i in range(0, len(variables_list)):
+                if dno[i] == "1":
+                    one_minterm += variables_list[i]
+
+                if dno[i] == "0":
+                    one_minterm += variables_list[i] + "'"  # negation sign
+
+            res.append(one_minterm)
+
+        res.sort()
+
+        print("DNO: " + expression + " \n " + " + ".join(res))
 
 
 if __name__ == '__main__':
 
-    expression_list=[]
-    variables_list=[]
+    expression_list = []
+    variables_list = []
 
     while True:
 
-        exprs = input("enter expression"+"\n")
+        exprs = input("enter expression" + "\n")
 
         if exprs == "exit" or exprs == "":
             print("ok bye!")
@@ -149,9 +218,7 @@ if __name__ == '__main__':
             expression_list.clear()
             variables_list.clear()
             continue
-           
-            
-        
+
         variables_list.extend(findvariables(exprs))
         variables_list = list(dict.fromkeys(variables_list))
         variables_list.sort()
@@ -162,21 +229,15 @@ if __name__ == '__main__':
         print(expression)
         print(variables_list)
 
-        results={}
-        results=calculate(expression_list,variables_list)
+        results = {}
+        results = calculate(expression_list, variables_list)
 
-        print("-"*30)
+        print("-" * 30)
         print(expression_list)
 
         printresults(variables_list, expression_list, results)
 
-       
+        minterms = get_minterms(variables_list, expression_list, results)
 
-
-
-
-       
-
-
-
-
+        printPDNO(minterms, variables_list)
+        printDNO(getDNO(minterms), variables_list)
